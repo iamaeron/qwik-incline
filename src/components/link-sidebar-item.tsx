dad1785245c9a8aae94e2825e815c9960777db0c
Aeron <senciljohnaeron@gmail.com>
2023-08-03 15:22:09 +0800

    meow

---
+++ src/components/link-sidebar-item.tsx
@@ -0,0 +1,34 @@
+import { component$ } from "@builder.io/qwik";
+import { Link, useLocation } from "@builder.io/qwik-city";
+import type {QRL} from "@builder.io/qwik";
+import type { JSX } from "@builder.io/qwik/jsx-runtime";
+
+interface LinkItem {
+  link: string;
+  label: string;
+  Icon: QRL<() => JSX.Element>
+}
+
+const LinkSidebarItem = component$<LinkItem>(({ link, label, Icon }) => {
+  const location = useLocation();
+  const active = link + '/' === location.url.pathname
+
+  return (
+    <Link
+      href={link}
+      class={`px-3 relative rounded-md text-sm font-medium py-1.5 w-full duration-150 ${
+        active
+          ? "text-[#E2C16B]"
+          : "hover:text-white text-[#958AB2] hover:bg-[#1E1536]"
+      }`}
+    >
+      <span class={`flex w-max items-center relative pr-8 ${active && "bg-[#160e27]"}`}>
+        {/* @ts-ignore */}
+        <Icon class="w-6 h-6 stroke-2 mr-3" />
+        {label}
+      </span>
+    </Link>
+  );
+});
+
+export default LinkSidebarItem;
--- src/root.tsx
+++ src/root.tsx
@@ -1,15 +1,19 @@
-import { component$ } from '@builder.io/qwik';
-import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister } from '@builder.io/qwik-city';
-import { RouterHead } from './components/router-head/router-head';
+import { component$ } from "@builder.io/qwik";
+import {
+  QwikCityProvider,
+  RouterOutlet,
+  ServiceWorkerRegister,
+} from "@builder.io/qwik-city";
+import { RouterHead } from "./components/router-head/router-head";
 
-import './global.css';
+import "./global.css";
 
 export default component$(() => {
   /**
    * The root of a QwikCity site always start with the <QwikCityProvider> component,
    * immediately followed by the document's <head> and <body>.
    *
-   * Dont remove the `<head>` and `<body>` elements.
+   * Don't remove the `<head>` and `<body>` elements.
    */
 
   return (
@@ -18,6 +22,11 @@ export default component$(() => {
         <meta charSet="utf-8" />
         <link rel="manifest" href="/manifest.json" />
         <RouterHead />
+        <link rel="preconnect" href="https://fonts.bunny.net" />
+        <link
+          href="https://fonts.bunny.net/css?family=be-vietnam-pro:300,400,500,600,700,800,900&display=swap"
+          rel="stylesheet"
+        />
       </head>
       <body lang="en">
         <RouterOutlet />
